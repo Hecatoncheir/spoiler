ea86721e9bffdb22f39f3a943e0f5a972088f425
vvostrikov <v.vostrikov@westcall.ru>
2019-10-22 13:14:22 +0300

    Adding Spoilers widget.

---
+++ lib/spoilers.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+import 'spoiler.dart';
+
+class Spoilers extends StatelessWidget {
+  final List<Spoiler> spoilers;
+
+  const Spoilers({this.spoilers});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
